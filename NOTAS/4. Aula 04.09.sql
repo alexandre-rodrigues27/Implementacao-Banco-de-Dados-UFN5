1b469830399895239d9159560b4afc314122404a
Alexandre Kikuchi Rodrigues <rodriguesxandy82@gmail.com>
2026-09-10 21:28:07 -0300

    Update 4. Aula 04.09.sql

--- NOTAS/4. Aula 04.09.sql
+++ NOTAS/4. Aula 04.09.sql
@@ -180,3 +180,143 @@ END
 
 CLOSE cursorFuncionario;
 DEALLOCATE cursorFuncionario;
+
+
+-- 1. Variáveis no SQL Server
+
+GO;
+DECLARE @NomeProduto VARCHAR(100),
+        @QtdEstoque INT,
+        @PrecoProduto DECIMAL(10,2);
+
+SET @NomeProduto = 'Notebook';
+SET @QtdEstoque = 15;
+SET @PrecoProduto = 2999.99;
+
+PRINT 'Produto: ' + @NomeProduto;
+PRINT 'Quantidade em estoque: ' + CAST(@QtdEstoque AS VARCHAR(10));
+PRINT 'Preço: R$' + CAST(@PrecoProduto AS VARCHAR(10));
+
+SELECT 
+    @NomeProduto AS NomeProduto,
+    @QtdEstoque AS QuantidadeEstoque,
+    @PrecoProduto AS PrecoProduto;
+GO;
+
+GO;
+DECLARE @SalarioBase DECIMAL(10,2),
+        @Bonus DECIMAL(10,2),
+        @SalarioTotal DECIMAL(10,2);
+
+SET @SalarioBase = 5000.00;
+SET @Bonus = 800.00;
+
+SET @SalarioTotal = @SalarioBase + @Bonus;
+
+PRINT 'Salário Total: R$' + CAST(@SalarioTotal AS VARCHAR(10));
+
+SELECT @SalarioTotal AS SalarioTotal;
+GO;
+
+
+-- 3. Estruturas Condicionais no SQL
+GO;
+DECLARE @Idade INT;
+
+SET @Idade = 20;
+
+IF (@Idade >= 18)
+	PRINT 'Maior de Idade';
+ELSE
+	PRINT 'Menor de Idade';
+GO;
+
+GO;
+DECLARE @NotaFinal DECIMAL(5,2);
+
+SET @NotaFinal = 82;
+
+IF (@NotaFinal >= 90)
+	PRINT 'Aprovado com Excelência';
+ELSE IF (@NotaFinal >= 70 AND @NotaFinal < 90)
+	PRINT 'Aprovado';
+ELSE IF (@NotaFinal >= 50 AND @NotaFinal < 70)
+	PRINT 'Em Recuperação';
+ELSE
+	PRINT 'Reprovado';
+GO;
+
+GO;
+DECLARE @Ano INT;
+
+SET @Ano = 2024;
+
+IF ((@Ano % 4 = 0 AND @Ano % 100 <> 0) OR (@Ano % 400 = 0))
+	PRINT 'Ano Bissexto';
+ELSE
+	PRINT 'Ano Comum';
+GO;
+
+
+-- 4. Loops no SQL
+GO;
+DECLARE @Contador INT = 1;
+
+WHILE @Contador <= 10
+BEGIN
+	PRINT 'Número: ' + CAST(@Contador AS VARCHAR(3));
+	SET @Contador = @Contador + 1;
+END
+GO;
+
+GO;
+DECLARE @Valor INT = 100;
+
+WHILE @Valor >= 50
+BEGIN
+	PRINT 'Valor: ' + CAST(@Valor AS VARCHAR(5));
+	SET @Valor = @Valor - 5;
+END
+GO;
+
+GO;
+DECLARE @Indice INT = 1,
+        @TotalProdutos INT,
+        @PrecoLimite DECIMAL(10,2),
+        @NomeProduto VARCHAR(100),
+        @PrecoProduto DECIMAL(10,2);
+
+SET @PrecoLimite = 100.00;
+
+SELECT @TotalProdutos = COUNT(*) FROM Produtos;
+
+WHILE @Indice <= @TotalProdutos
+BEGIN
+	SELECT 
+		@NomeProduto = Nome,
+		@PrecoProduto = Preco
+	FROM (
+		SELECT 
+			Nome, 
+			Preco,
+			ROW_NUMBER() OVER (ORDER BY Nome) AS NumeroLinha
+		FROM Produtos
+	) AS ProdutosNumerados
+	WHERE NumeroLinha = @Indice;
+
+	IF (@PrecoProduto > @PrecoLimite)
+		PRINT 'Produto: ' + @NomeProduto + ' - Preço: R$' + CAST(@PrecoProduto AS VARCHAR(10));
+
+	SET @Indice = @Indice + 1;
+END
+GO;
+
+GO;
+DECLARE @Numero INT = 2;
+
+WHILE @Numero <= 1000
+BEGIN
+	PRINT 'Número: ' + CAST(@Numero AS VARCHAR(10));
+	SET @Numero = @Numero * 2;
+END
+GO;
